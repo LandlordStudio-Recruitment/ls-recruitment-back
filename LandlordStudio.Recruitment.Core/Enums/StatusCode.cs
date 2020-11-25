6ff931a02c9eccaa959c56708c2b1d2bac66d652
Peter Kang <peter@niesh.co.nz>
2020-11-25 21:40:45 +1300

    Add custom exception

---
+++ LandlordStudio.Recruitment.Core/Enums/StatusCode.cs
@@ -0,0 +1,14 @@
+﻿
+namespace LandlordStudio.Recruitment.Core.Enums
+{
+    public enum StatusCode
+    {
+        Unknown = 0,
+        BadRequest = 400,
+        Forbidden = 403,
+        NotFound = 404,
+        Conflict = 409,
+        ImATeapot = 418,
+        InternalServerError = 500
+    }
+}
---
+++ LandlordStudio.Recruitment.Core/Exceptions/BaseException.cs
@@ -0,0 +1,14 @@
+﻿using LandlordStudio.Recruitment.Core.Enums;
+using System;
+
+namespace LandlordStudio.Recruitment.Core.Exceptions
+{
+    public class BaseException : Exception
+    {
+        public BaseException(StatusCode statusCode, string message) : base(message)
+        {
+            StatusCode = statusCode;
+        }
+        public StatusCode StatusCode { get; set; }
+    }
+}
